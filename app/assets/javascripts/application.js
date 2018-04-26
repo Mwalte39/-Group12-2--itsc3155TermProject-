19ec02208c8792eb36ef934cee4b2ebf122695ae
Zuberi Reid <zreid1@uncc.edu>
2018-04-26 05:10:39 +0000

    now removes null polls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,36 +19,53 @@
 var ready = false;
 
 $(document).on("turbolinks:load", () => {
-
+    //DEFINE SOME THINGS
     var $graph = $("#graph");
     var $graphtitles = $("#graphtitles");
     var $option = $(".option");
+    var shown = false;
 
     var valbar1 = $("#bar1").data("bar1num");
     var valbar2 = $("#bar2").data("bar2num");
     var valbar3 = $("#bar3").data("bar3num");
     var valbar4 = $("#bar4").data("bar4num");
 
+
+    //CALCULATE SOME THINGS
     var total = valbar1 + valbar2 + valbar3 + valbar4;
     var relative_height = 490 / total;
-
-    var shown = false;
 
     $("#bar1").css("height", relative_height * valbar1);
     $("#bar2").css("height", relative_height * valbar2);
     $("#bar3").css("height", relative_height * valbar3);
     $("#bar4").css("height", relative_height * valbar4);
     
-    $option.hide();
+    //CLEAN SOME THINGS
+    if ($("#op1").data("op1") == ""){
+        $("#op1").remove();
+        $("#bar1").remove();
+        $("#title1").remove();
+    }
+    if ($("#op2").data("op2") == ""){
+        $("#op2").remove();
+        $("#bar2").remove();
+        $("#title2").remove();
+    }
+    if ($("#op3").data("op3") == ""){
+        $("#op3").remove();
+        $("#bar3").remove();
+        $("#title3").remove();
+    }
+    if ($("#op4").data("op4") == ""){
+        $("#op4").remove();
+        $("#bar4").remove();
+        $("#title4").remove();
+    }
+    
+    //PREPARE THE STAGE
     $graph.add($graphtitles).hide();
-    
-    $option.css("display", "flex");
-    
-    $option.each(function(index) {
-        $(this).delay(index * 200).fadeIn(400);
-    });
 
-
+    //LET THE SHOW BEGIN
     $("#graphtoggle").on("click", () => {
         if (shown == true) {
             $graph.add($graphtitles).stop(true, true).fadeOut(100, () => {
