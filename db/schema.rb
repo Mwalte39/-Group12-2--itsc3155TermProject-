29463054fe95eb63a784daf965f391479992a9da
Mwalte39 <mwalte39@uncc.edu>
2018-04-27 15:05:54 +0000

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180413022023) do
+ActiveRecord::Schema.define(version: 20180427150505) do
 
   create_table "polls", force: :cascade do |t|
     t.string "question"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 20180413022023) do
     t.string "answer2"
     t.string "answer3"
     t.string "answer4"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
