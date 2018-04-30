df4d1517c1928b93a037c57793ee3f865beabebf
Mwalte39 <mwalte39@uncc.edu>
2018-04-30 02:01:44 +0000

    Finalize google sign in functionality

--- db/migrate/20180413022023_create_votes.rb
+++ db/migrate/20180413022023_create_votes.rb
@@ -3,6 +3,7 @@ class CreateVotes < ActiveRecord::Migration[5.1]
     create_table :votes do |t|
       t.integer :selection
       t.references :poll, foreign_key: true
+      t.string :userName
 
       t.timestamps
     end
